ee29cbee98699bbabab32ce41469f04bd15ffb8a
morchid <212637329@qq.com>
2023-12-11 19:53:34 +0800

    实体类型：设备

---
+++ entity/Device.java
@@ -0,0 +1,15 @@
+public class Device {
+    /** 设备编号 */
+    private String id;
+    /** 设备名称 */
+    private String name;
+    /** 领用人 */
+    private String who;
+    /** 设备类型 */
+    private String type;
+    /** 设备状态（是否借出）*/
+    private boolean isBorrowed;
+    /** 是否是报废设备 */
+    private boolean isDeprecated;
+
+}
