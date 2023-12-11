814a2e263f3cb930c9be3233d931d2fabcea3f4f
morchid <212637329@qq.com>
2023-12-11 19:55:01 +0800

    设备的构造器与 Gs、Ss 方法

--- entity/Device.java
+++ entity/Device.java
@@ -12,4 +12,62 @@ public class Device {
     /** 是否是报废设备 */
     private boolean isDeprecated;
 
+    public Device(String id, String name, String who, String type, boolean isBorrowed, boolean isDeprecated) {
+        this.id = id;
+        this.name = name;
+        this.who = who;
+        this.type = type;
+        this.isBorrowed = isBorrowed;
+        this.isDeprecated = isDeprecated;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWho() {
+        return who;
+    }
+
+    public void setWho(String who) {
+        this.who = who;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public boolean isBorrowed() {
+        return isBorrowed;
+    }
+
+    public void setBorrowed(boolean isBorrowed) {
+        this.isBorrowed = isBorrowed;
+    }
+
+    public boolean isDeprecated() {
+        return isDeprecated;
+    }
+
+    public void setDeprecated(boolean isDeprecated) {
+        this.isDeprecated = isDeprecated;
+    }
+
+    
 }
