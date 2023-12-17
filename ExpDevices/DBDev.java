37ca5d371fd8331773cc8493255ef73bab6d74df
morchid <212637329@qq.com>
2023-12-17 14:02:37 +0800

    Dev

--- ExpDevices/DBDev.java
+++ ExpDevices/DBDev.java
@@ -1,11 +1,14 @@
 package ExpDevices;
 
+import java.util.Set;
+
 import ExpDevices.DAO.DeviceImpl;
 import ExpDevices.entity.*;
 
 public class DBDev {
     public static void main(String[] args) {
         DeviceImpl deviceImpl = new DeviceImpl();
+        Set<Device> devices = deviceImpl.getDevices();
         // Device device1 = new Device("001", "a1", "mo", "phy", false,
         //         false);
         // Device device2 = new Device("002", "a2", "mo2", "chs", false,
@@ -14,7 +17,8 @@ public class DBDev {
         // deviceImpl.add(device2);
         // deviceImpl.add(null);
 
-        System.out.println(deviceImpl.getDeviceByID("001"));
-        System.out.println(deviceImpl.getDeviceByID("002"));
+        for (Device device : devices) {
+            System.out.println(device);
+        }
     }
 }
